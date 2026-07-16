85c89acde796322ab67e7d959e5608f6229f94c3
Sarnog <dannywous@gmail.com>
2026-07-16 09:36:19 +0200

    Initial project structure

---
+++ custom_components/cure_afvalbeheer/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Cure Afvalbeheer integration."""
+
+DOMAIN = "cure_afvalbeheer"
+
+NAME = "Cure Afvalbeheer"
+
+MANUFACTURER = "Cure Afvalbeheer"
+
+DEFAULT_SCAN_INTERVAL = 3600  # seconden
